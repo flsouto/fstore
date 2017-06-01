761c5e7c664e2dcda340915a3060ffeaaee00fce
Fabio <ti@traca.com.br>
2017-06-01 12:02:23 -0300

    Add test roadmap

---
+++ tests/FstoreTest.php
@@ -0,0 +1,19 @@
+// Fstore //////////////
+// create database
+// get save dir
+// get a table
+
+// Table /////////
+// insert
+// update
+// delete
+// get all ids
+// get first 10 ids
+// get 10 last ids
+// get row by id
+
+// Query ///////
+// fetch rows matching criteria (filter)
+// limit result (all, last, first)
+// fetch values of a single column
+// fetch only the ids matching the criteria
