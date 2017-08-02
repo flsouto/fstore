acf11cace8c6fbeff49c647edbb6187dc117005e
Fabio <ti@traca.com.br>
2017-08-02 10:35:06 -0300

    Add mdx annotations

--- tests/FstoreTest.php
+++ tests/FstoreTest.php
@@ -13,9 +13,13 @@ A database is nothing more than a directory. So all you have to do is specify th
 directory when creating a database instance. If the directory does not exist, it will be created
 when the first row is inserted.
 
+#mdx:CreateDatabase
+
 */
     function testCreateDatabase(){
+        #mdx:CreateDatabase
         $db = new Fstore(__DIR__.'/test_db');
+        #/mdx
         $this->assertEquals(__DIR__.'/test_db', $db->dir());
     }
 
@@ -35,11 +39,15 @@ when the first row is inserted.
 You can get a table instance from a database by calling the `table` method. If the table does not exist, it will be
 created once the first row is inserted.
 
-*/    
+#mdx:GetTable
+
+*/
     function testGetTable(){
+        #mdx:GetTable
         $db = new Fstore(__DIR__.'/test_db');
         // creates users table on the fly
         $products = $db->table('products');
+        #/mdx
         $this->assertInstanceOf(FstoreTable::class, $products);
     }
 
@@ -49,9 +57,14 @@ created once the first row is inserted.
 The table object provides an `insert` method which accepts an associative array and returns the automatically generated
 row id:
 
-*/    
-    
+#mdx:InsertAndGet
+
+Outputs:
+
+#mdx:InsertAndGet -o
+*/
     function testInsertAndGet(){
+        #mdx:InsertAndGet
         $db = new Fstore(__DIR__.'/test_db');
         $products = $db->table('products');
         $id = $products->insert([
@@ -61,7 +74,7 @@ row id:
         ]);
 
         $new_row = $products->get($id);
-
+        #/mdx print_r($new_row)
         $this->assertEquals([
             'name' => 'Pencil',
             'description' => 'Can be used to write things down.',
@@ -77,8 +90,14 @@ But, in order to avoid conflicts when inserting multiple rows on the same table 
 which guarantees that the ids will always be different, even if inserted on the same nanosecond! So, in order to take the corrrect date/time from this hash you have to remove those extra numbers.
 The good news is that there is a `$table->date` method which does just that for you:
 
-*/    
+#mdx:GetDateBasedOnId
+
+Outputs:
+
+#mdx:GetDateBasedOnId -o
+*/
     function testGetDateBasedOnId(){
+        #mdx:GetDateBasedOnId
         $db = new Fstore(__DIR__.'/test_db');
         $products = $db->table('products');
         $id = $products->insert([
@@ -88,6 +107,7 @@ The good news is that there is a `$table->date` method which does just that for 
         ]);
         
         $date = $products->date('d/m/Y H:i', $id);
+        #/mdx echo $date
         $this->assertEquals($date, date('d/m/Y H:i'));
     }
 
@@ -96,9 +116,15 @@ The good news is that there is a `$table->date` method which does just that for 
 Updating a table record is pretty straightforward: just call `update` and pass it an associative array followed by
 the row id:
 
-*/    
+#mdx:Update
+
+Outputs:
+
+#mdx:Update -o
+*/
     function testUpdate(){
 
+        #mdx:Update
         $db = new Fstore(__DIR__.'/test_db');
         $products = $db->table('products');
 
@@ -111,7 +137,7 @@ the row id:
         $products->update(['price'=>1.5], $id);
 
         $row = $products->get($id);
-
+        #/mdx print_r($row)
         $this->assertEquals(1.5, $row['price']);
 
     }
@@ -120,9 +146,12 @@ the row id:
 ### Deleting a Record
 To delete a record simply call `$table->delete($id)`, see example:
 
-*/    
+#mdx:Delete
+
+*/
     function testDelete(){
 
+        #mdx:Delete
         $db = new Fstore(__DIR__.'/test_db');
         $products = $db->table('products');
 
@@ -133,7 +162,7 @@ To delete a record simply call `$table->delete($id)`, see example:
         ]);
 
         $products->delete($id);
-
+        #/mdx
         $this->expectException(\InvalidArgumentException::class);
 
         $products->get($id);
@@ -143,9 +172,15 @@ To delete a record simply call `$table->delete($id)`, see example:
 ### Retrieving all IDs from a Table
 Use `$table->ids()` to get all ids:
 
+#mdx:GetAllIds
+
+Outputs:
+
+#mdx:GetAllIds -o
 */
     function testGetAllIds(){
 
+        #mdx:GetAllIds
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -158,16 +193,23 @@ Use `$table->ids()` to get all ids:
         }
 
         $ids = $table->ids();
-
+        #/mdx print_r($ids)
         $this->assertEquals($ids, $inserted_ids);
 
     }
 /* 
 ### Retrieving only first 10 IDs from a Table
 Use `$table->ids(X)` to get the first X ids:
+
+#mdx:GetFirstXIds
+
+Outputs:
+
+#mdx:GetFirstXIds -o
 */
     function testGetFirstXIds(){
 
+        #mdx:GetFirstXIds
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -180,7 +222,7 @@ Use `$table->ids(X)` to get the first X ids:
         }
 
         $first_10_ids = $table->ids(10);
-
+        #/mdx print_r($first_10_ids)
         $this->assertEquals(array_slice($inserted_ids,0,10), $first_10_ids);
 
     }
@@ -190,8 +232,16 @@ Use `$table->ids(X)` to get the first X ids:
 Use `$table->ids(-X)` to get the last X ids:
 
 */
+/*
+#mdx:GetLastXIds
+
+Outputs:
+
+#mdx:GetLastXIds -o
+*/
     function testGetLastXIds(){
 
+        #mdx:GetLastXIds
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -204,7 +254,7 @@ Use `$table->ids(-X)` to get the last X ids:
         }
 
         $last_10_ids = $table->ids(-10);
-
+        #/mdx print_r($last_10_ids)
         $this->assertEquals(array_slice(array_reverse($inserted_ids),0,10), $last_10_ids);
 
     }
@@ -215,9 +265,15 @@ The table object provides a query builder which can be created via `$table->quer
 This query builder allows us to filter results by means of a `$query->filter()` function
 which accepts a callback. The following example illustrates this better:
 
-*/    
+#mdx:FetchRowsWithFilter
+
+Outputs:
+
+#mdx:FetchRowsWithFilter -o
+*/
     function testFetchRowsWithFilter(){
 
+        #mdx:FetchRowsWithFilter
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -238,6 +294,7 @@ which accepts a callback. The following example illustrates this better:
         // fetch those rows
         $rows = $q->rows();
         
+        #/mdx print_r($rows)
         $this->assertEquals(array_column($rows,'letter'),['a','b','f','i','o']);
         
     }
@@ -245,10 +302,17 @@ which accepts a callback. The following example illustrates this better:
 /* 
 ### Querying The Last X Rows...
 Call the `limit` method on a query object and pass it a negative number. See example:
-*/    
-    
+
+#mdx:LastXRows
+
+Outputs:
+
+#mdx:LastXRows -o
+
+*/
     function testLastXRows(){
 
+        #mdx:LastXRows
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -267,6 +331,7 @@ Call the `limit` method on a query object and pass it a negative number. See exa
 
         // fetch them
         $rows = $q->rows();
+        #/mdx print_r($rows)
 
         $this->assertEquals(
             array_reverse(['q','r','s','t','u','v','w', 'x', 'y', 'z']),
@@ -279,9 +344,16 @@ Call the `limit` method on a query object and pass it a negative number. See exa
 ### Fetching only one column from the result
 Call the `values` method on a query object passing it the name of the desired column.
 This will return an array of all values of that column. See example:
+
+#mdx:FetchValues
+
+Outputs:
+
+#mdx:FetchValues -o
 */
     function testFetchValues(){
 
+        #mdx:FetchValues
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -298,7 +370,7 @@ This will return an array of all values of that column. See example:
 
         // fetch values of 'letter' column
         $values = $q->values('letter');
-
+        #/mdx print_r($values)
         $this->assertEquals(
             range('a','j'),
             array_values($values)
@@ -309,9 +381,15 @@ This will return an array of all values of that column. See example:
 ### Fetching only the ids from a result
 Call the `ids` method on a query object:
 
+#mdx:FetchIds
+
+Outputs:
+
+#mdx:FetchIds -o
 */
     function testFetchIds(){
 
+        #mdx:FetchIds
         $db = new Fstore(__DIR__.'/test_db');
         $table = $db->table('alphabet');
 
@@ -332,6 +410,7 @@ Call the `ids` method on a query object:
 
         // fetch only the ids
         $ids = $q->ids();
+        #/mdx print_r($ids)
 
         $expected = [];
         foreach(str_split('fabio') as $letter){
@@ -339,7 +418,6 @@ Call the `ids` method on a query object:
         }
         sort($ids);
         sort($expected);
-
         $this->assertEquals(
             $expected,
             $ids
